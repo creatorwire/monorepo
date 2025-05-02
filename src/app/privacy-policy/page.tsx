7bf537ab8dc495a49dd92519f714f9a50b049be5
asta-otaku <afolabiibrahim08@gmail.com>
2025-05-02 21:21:44 +0100

    chore: local policy pages

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Page() {
+  return (
+    <div className="flex flex-col bg-white items-center justify-center w-full max-screen-2xl pb-12 mx-auto scrollbar-hide">
+      <iframe
+        src="/docs/privacy-policy.html"
+        className="max-w-7xl w-full h-[90vh] bg-white my-12 p-4"
+        title="Privacy Policy"
+      />
+    </div>
+  );
+}
+
+export default Page;
