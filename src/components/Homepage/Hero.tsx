196951c42cd48079fc79687d8828699c834e9520
asta-otaku <afolabiibrahim08@gmail.com>
2025-05-02 08:00:05 +0100

    responsive hero fix

--- src/components/Homepage/Hero.tsx
+++ src/components/Homepage/Hero.tsx
@@ -6,7 +6,7 @@ import React from "react";
 export default function Hero() {
   return (
     <div className="px-2 md:px-4 py-2 md:py-4 bg-white h-screen">
-      <div className="relative bg-gray-400 bg-opacity-20 backdrop-blur-lg flex min-h-screen h-full items-center justify-center rounded-2xl overflow-hidden">
+      <div className="relative bg-gray-400 bg-opacity-20 backdrop-blur-lg flex max-h-[98vh] h-full items-center justify-center rounded-2xl overflow-hidden">
         <div className="absolute inset-0 pointer-events-none">
           <div className="absolute top-10 left-0 w-[40vw] aspect-square bg-purple-400 rounded-full filter blur-3xl opacity-70 animate-blob"></div>
           <div className="absolute top-1/2 right-0 w-[45vw] aspect-square bg-pink-400 rounded-full filter blur-3xl opacity-60 animate-blob2"></div>
@@ -15,7 +15,7 @@ export default function Hero() {
 
         <div className="max-w-screen-2xl mx-auto h-full w-full flex flex-col items-center justify-center relative">
           {/* Content */}
-          <div className="relative z-10 text-center px-4 max-w-2xl w-full flex flex-col gap-8 items-center py-8 md:mb-12">
+          <div className="relative z-10 text-center px-4 max-w-2xl w-full flex flex-col gap-4 items-center max-h-[600px] overflow-auto scrollbar-hide py-8 md:mb-12 md:-mt-12">
             <img
               src="/logo.svg"
               alt="creatorwire"
@@ -27,11 +27,11 @@ export default function Hero() {
               className="mx-auto md:hidden w-[12rem]"
             />
             <div className="w-full text-center">
-              <h1 className="text-[#121212] font-poppins font-extrabold text-3xl md:text-4xl lg:text-[7vh] xl:text-6xl leading-tight uppercase tracking-tighter">
+              <h1 className="text-[#121212] font-poppins font-extrabold text-3xl md:text-4xl lg:text-5xl xl:text-6xl leading-tight uppercase tracking-tighter">
                 Getting creators paid early or on time, every time — with zero
                 admin stress
               </h1>
-              <p className="font-medium text-[#626262] md:text-lg lg:text-xl mt-4">
+              <p className="font-medium text-[#626262] md:text-lg lg:text-xl">
                 Fast, reliable payments for content creators, creative
                 entrepreneurs and freelancers, with full visibility and simple
                 tracking for brands and agencies.
@@ -46,7 +46,7 @@ export default function Hero() {
           </div>
 
           {/* Bottom nav pill */}
-          <div className="absolute bottom-2 xl:bottom-[5%] left-1/2 transform -translate-x-1/2 bg-white rounded-xl shadow-gray-400 shadow-md py-2 px-6 hidden md:flex items-center justify-between min-w-[35%] gap-4">
+          <div className="absolute bottom-2 xl:bottom-[3vh] left-1/2 transform -translate-x-1/2 bg-white rounded-xl shadow-gray-400 shadow-md py-2 px-6 hidden md:flex items-center justify-between min-w-[35%] gap-4">
             <Link href="/">
               <img src="/logoIcon.svg" alt="" />
             </Link>
